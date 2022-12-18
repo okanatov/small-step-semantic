2d77388ad4efa939583d467f0833b3df9e8623bc
Oleg Kanatov <okanatov@gmail.com>
2022-12-18 14:03:46 +0300

    While

--- main.scm
+++ main.scm
@@ -11,24 +11,13 @@
   "assign.scm"
   "if.scm"
   "sequence.scm"
+  "while.scm"
   )
 
-(define env (hash-set
-	      (hash-set (hash) "x" 3)
-	      "y" 4))
 
-;(define f (machine-step (Assign "x" (LessThan
-;				      (Number 5)
-;				      (Add (Multiply (Variable "x") (Number 2))
-;					   (Multiply (Number 3) (Variable "y")))))
-;			env))
-
-;(define f (machine-step (If (Variable "x") (Assign "y" (Number 1)) (Assign "y" (Number 2)))
-;			(hash-set (hash) "x" #f)))
-
-(define f (machine-step (Sequence (Assign "x" (Add (Number 1) (Number 1)))
-				  (Assign "y" (Add (Variable "x") (Number 3))))
-			(hash-set (hash) "x" #f)))
+(define f (machine-step (While (LessThan (Variable "x") (Number 5))
+			       (Assign "x" (Multiply (Variable "x") (Number 3))))
+			(hash-set (hash) "x" 1)))
 
 (displayln f)
 
